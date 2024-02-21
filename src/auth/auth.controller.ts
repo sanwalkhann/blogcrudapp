3e3268c7ff677f46ad9c78a3e24f4116cd984799
Sanwal Khan <sanwal_khan_intern@netixsol.com>
2024-02-22 00:51:21 +0500

    asd

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -8,7 +8,9 @@ import {
   Delete,
 } from '@nestjs/common';
 import { AuthService } from './auth.service';
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 import { signUpDto } from 'src/dto/signup.dto';
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 import { logInDto } from 'src/dto/login.dto';
 import { User } from 'src/schemas/user.schema';
 @Controller('auth')
@@ -16,11 +18,13 @@ export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Post('signup')
-  signUp(@Body() signUpDto: signUpDto): Promise<{message: string;}> {
+  signUp(@Body() signUpDto: signUpDto): Promise<{ message: string }> {
     return this.authService.signUp(signUpDto);
   }
   @Post('login')
-  login(@Body() logInDto: logInDto): Promise<{message: string,token:string, user: User}> {
+  login(
+    @Body() logInDto: logInDto,
+  ): Promise<{ message: string; token: string; user: User }> {
     return this.authService.logIn(logInDto);
   }
 }
